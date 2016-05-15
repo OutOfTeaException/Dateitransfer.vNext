c19a002114e07581ee626a170533dec0c45852c7
Timo Kuper <timo.kuper@outlook.com>
2016-05-15 15:45:53 +0200

    Create initial Solution

---
+++ Dateitransfer.vNext.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dateitransfer.vNext.Lib
+{
+    public class Class1
+    {
+    }
+}

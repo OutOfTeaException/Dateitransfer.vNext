e2c0bf8c10a8f9a70327d8d8f405f65cceeca66d
Timo Kuper <timo.kuper@gmail.com>
2016-05-15 16:19:38 +0200

    Added DbContext

---
+++ Dateitransfer.vNext.Lib/Data/DateitransferContext.cs
@@ -0,0 +1,17 @@
+﻿using Dateitransfer.vNext.Lib.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dateitransfer.vNext.Lib.Data
+{
+    public class DateitransferContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Input> Inputs { get; set; }
+        public DbSet<Output> Outputs { get; set; }
+    }
+}
